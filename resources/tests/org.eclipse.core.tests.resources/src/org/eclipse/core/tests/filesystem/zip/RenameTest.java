1f1bc60beaaf0491cb39232a014849cc07c97f21
Michael Bangas <michael.bangas@vector.com>
2024-02-26 13:17:05 +0100

    Added Rename tests for Archives
    Please note that rename for IFolder and IFile is performed by moving the file in place with a different name

--- resources/tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/filesystem/zip/RenameTest.java
+++ resources/tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/filesystem/zip/RenameTest.java
@@ -13,14 +13,14 @@
  *******************************************************************************/
 package org.eclipse.core.tests.filesystem.zip;
 
+import static org.eclipse.core.tests.filesystem.zip.ZipFileSystemTestUtil.ensureDoesNotExist;
 import static org.eclipse.core.tests.filesystem.zip.ZipFileSystemTestUtil.ensureExists;
-import static org.junit.Assert.assertTrue;
+import static org.eclipse.core.tests.filesystem.zip.ZipFileSystemTestUtil.getMonitor;
 import static org.mockito.Mockito.mock;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.examples.filesystem.CollapseZipHandler;
 import org.eclipse.ui.examples.filesystem.ExpandZipHandler;
 import org.junit.After;
 import org.junit.Before;
@@ -29,7 +29,7 @@ import org.junit.Test;
 /**
  *
  */
-public class CollapseTest {
+public class RenameTest {
 
 	@Before
 	public void setup() throws Exception {
@@ -50,25 +50,27 @@ public class CollapseTest {
 		ensureExists(virtualFolder);
 	}
 
-	private void collapseZipFile(IFolder folder) throws Exception {
-		CollapseZipHandler collapseZipHandler = new CollapseZipHandler();
-		Shell shell = mock(Shell.class);
-		collapseZipHandler.collapseZip(folder, shell);
-		IFile zipFile = ZipFileSystemTestSetup.project.getFile(folder.getName());
-		ensureExists(zipFile);
+	@Test
+	public void testRenameZipArchive() throws Exception {
+		// IFolder is renamed by moving with the new path
+		IFolder virtualFolder = ZipFileSystemTestSetup.project
+				.getFolder(ZipFileSystemTestSetup.ZIP_FILE_VIRTUAL_FOLDER_NAME);
+		IFolder virtualFolder2 = ZipFileSystemTestSetup.project
+				.getFolder(ZipFileSystemTestSetup.ZIP_FILE_VIRTUAL_FOLDER_NAME + "Renamed");
+		virtualFolder.move(virtualFolder2.getFullPath(), false, getMonitor());
+		ensureExists(virtualFolder2);
+		ensureDoesNotExist(virtualFolder);
 	}
 
 	@Test
-	public void testCollapseZipFile() throws Exception {
-		IFolder virtualFolder = ZipFileSystemTestSetup.project
-				.getFolder(ZipFileSystemTestSetup.ZIP_FILE_VIRTUAL_FOLDER_NAME);
-		ensureExists(virtualFolder);
-		collapseZipFile(virtualFolder);
-		IFile zipFile = ZipFileSystemTestSetup.project.getFile(ZipFileSystemTestSetup.ZIP_FILE_VIRTUAL_FOLDER_NAME);
-		// Don't use Utility method ensureDoesNotExist because the fileStore is still
-		// available after collapse. The fileStore is the File itself in the local file
-		// system that still exists after collapse.
-		assertTrue("folder was not properly deleted: " + virtualFolder, !virtualFolder.exists());
-		ensureExists(zipFile);
+	public void testRenameFileInsideOfZip() throws Exception {
+		// IFolder is renamed by moving with the new path
+		IFile textFile = ZipFileSystemTestSetup.project.getFile(
+				ZipFileSystemTestSetup.ZIP_FILE_VIRTUAL_FOLDER_NAME + "/" + ZipFileSystemTestSetup.TEXT_FILE_NAME);
+		IFile newTextFile = ZipFileSystemTestSetup.project.getFile(ZipFileSystemTestSetup.ZIP_FILE_VIRTUAL_FOLDER_NAME
+				+ "/" + ZipFileSystemTestSetup.TEXT_FILE_NAME + "Renamed");
+		textFile.move(newTextFile.getFullPath(), false, getMonitor());
+		ensureExists(newTextFile);
+		ensureDoesNotExist(textFile);
 	}
 }
