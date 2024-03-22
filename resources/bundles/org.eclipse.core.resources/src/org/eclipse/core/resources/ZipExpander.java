670cc9a7cdef94f21beb60144f9a6aea3a215b6e
Michael Bangas <michael.bangas@vector.com>
2024-03-22 11:24:15 +0100

    ZipExpander throws error when compression method not suppoted
    ZipExpander now checks before hand if a new fileSystem can be created. In the case of unsupported compression method this is not possible and an IOException is thrown.

--- resources/bundles/org.eclipse.core.resources/src/org/eclipse/core/resources/ZipExpander.java
+++ resources/bundles/org.eclipse.core.resources/src/org/eclipse/core/resources/ZipExpander.java
@@ -1,18 +1,38 @@
 package org.eclipse.core.resources;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.file.FileSystems;
+import java.util.HashMap;
+import java.util.Map;
+import org.eclipse.core.filesystem.EFS;
+import org.eclipse.core.filesystem.IFileStore;
+import org.eclipse.core.filesystem.URIUtil;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 
 /**
  * @since 3.21
  *
  */
 public class ZipExpander {
-	public static void expandZip(IFile file) throws URISyntaxException, CoreException {
-			URI zipURI = new URI("zip", null, "/", file.getLocationURI().toString(), null); //$NON-NLS-1$ //$NON-NLS-2$
-			IFolder link = file.getParent().getFolder(IPath.fromOSString(file.getName()));
-			link.createLink(zipURI, IResource.REPLACE, null);
+	public static void expandZip(IFile file) throws URISyntaxException, CoreException, IOException {
+		URI zipURI = new URI("zip", null, "/", file.getLocationURI().toString(), null); //$NON-NLS-1$ //$NON-NLS-2$
+		Map<String, Object> env = new HashMap<>();
+		env.put("create", "false"); //$NON-NLS-1$ //$NON-NLS-2$
+		IFileStore fileStore = EFS.getStore(URIUtil.toURI(file.getLocation()));
+		URI jarUri = new URI("jar:" + fileStore.toURI().toString() + "!/"); //$NON-NLS-1$ //$NON-NLS-2$
+		// Try creating file System to catch errors before actual execution
+		FileSystems.newFileSystem(jarUri, env);
+
+		IFolder link = file.getParent().getFolder(IPath.fromOSString(file.getName()));
+		link.createLink(zipURI, IResource.REPLACE, null);
+		if (link.members().length == 0) {
+			ZipCollapser.collapseZip(link);
+			throw new CoreException(new Status(IStatus.ERROR, "YourPluginID", "compression method not supported")); //$NON-NLS-1$ //$NON-NLS-2$
+		}
 	}
 }
