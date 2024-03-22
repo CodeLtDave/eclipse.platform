567a989a35ebd741dec00c81786b3b9f0a9cf30a
Michael Bangas <michael.bangas@vector.com>
2024-03-22 12:03:10 +0100

    new test for expanding archive with compression method Enhanced-Deflate

--- resources/tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/filesystem/zip/AllZipFileSystemTests.java
+++ resources/tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/filesystem/zip/AllZipFileSystemTests.java
@@ -21,7 +21,7 @@ import org.junit.runners.Suite;
  */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({ CollapseTest.class, CopyTest.class, CreateTest.class, DeleteTest.class, MoveTest.class,
-		RenameTest.class, SetupTest.class })
+		RenameTest.class, SetupTest.class, IllegalCompressionMethodTest.class })
 public class AllZipFileSystemTests {
 }
 
