2a381dc1c02ab534ffe0def264afb101fd2e2f5e
Michael Bangas <michael.bangas@vector.com>
2024-03-26 11:48:47 +0100

    Avoiding Null Pointer Exception in ZipExpander

--- resources/bundles/org.eclipse.core.resources/src/org/eclipse/core/resources/ZipExpander.java
+++ resources/bundles/org.eclipse.core.resources/src/org/eclipse/core/resources/ZipExpander.java
@@ -3,6 +3,7 @@ package org.eclipse.core.resources;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
 import java.util.HashMap;
 import java.util.Map;
@@ -21,12 +22,14 @@ public class ZipExpander {
 		URI zipURI = new URI("zip", null, "/", file.getLocationURI().toString(), null); //$NON-NLS-1$ //$NON-NLS-2$
 		Map<String, Object> env = new HashMap<>();
 		env.put("create", "false"); //$NON-NLS-1$ //$NON-NLS-2$
+		if (file.getLocation() != null) {
 		IFileStore fileStore = EFS.getStore(URIUtil.toURI(file.getLocation()));
 		URI jarUri = new URI("jar:" + fileStore.toURI().toString() + "!/"); //$NON-NLS-1$ //$NON-NLS-2$
 		// Try creating file System to catch errors before actual execution
-		FileSystems.newFileSystem(jarUri, env).close();
-
-		IFolder link = file.getParent().getFolder(IPath.fromOSString(file.getName()));
-		link.createLink(zipURI, IResource.REPLACE, null);
+		try (FileSystem __ = FileSystems.newFileSystem(jarUri, env)) {
+			IFolder link = file.getParent().getFolder(IPath.fromOSString(file.getName()));
+			link.createLink(zipURI, IResource.REPLACE, null);
+		}
+	}
 	}
 }
