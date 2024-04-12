5207125d6918f4490639c92cdab3b3570e45d4e0
Michael Bangas <michael.bangas@vector.com>
2024-04-12 09:51:44 +0200

    fix indentation

--- resources/bundles/org.eclipse.core.resources/src/org/eclipse/core/resources/ZipExpander.java
+++ resources/bundles/org.eclipse.core.resources/src/org/eclipse/core/resources/ZipExpander.java
@@ -1,7 +1,15 @@
 package org.eclipse.core.resources;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.util.HashMap;
+import java.util.Map;
+import org.eclipse.core.filesystem.EFS;
+import org.eclipse.core.filesystem.IFileStore;
+import org.eclipse.core.filesystem.URIUtil;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 
@@ -10,9 +18,18 @@ import org.eclipse.core.runtime.IPath;
  *
  */
 public class ZipExpander {
-	public static void expandZip(IFile file) throws URISyntaxException, CoreException {
+	public static void expandZip(IFile file) throws URISyntaxException, CoreException, IOException {
 		URI zipURI = new URI("zip", null, "/", file.getLocationURI().toString(), null); //$NON-NLS-1$ //$NON-NLS-2$
-		IFolder link = file.getParent().getFolder(IPath.fromOSString(file.getName()));
-		link.createLink(zipURI, IResource.REPLACE, null);
+		Map<String, Object> env = new HashMap<>();
+		env.put("create", "false"); //$NON-NLS-1$ //$NON-NLS-2$
+		if (file.getLocation() != null) {
+			IFileStore fileStore = EFS.getStore(URIUtil.toURI(file.getLocation()));
+			URI jarUri = new URI("jar:" + fileStore.toURI().toString() + "!/"); //$NON-NLS-1$ //$NON-NLS-2$
+			// Try creating file System to catch errors before actual execution
+			try (FileSystem __ = FileSystems.newFileSystem(jarUri, env)) {
+				IFolder link = file.getParent().getFolder(IPath.fromOSString(file.getName()));
+				link.createLink(zipURI, IResource.REPLACE, null);
+			}
+		}
 	}
 }
