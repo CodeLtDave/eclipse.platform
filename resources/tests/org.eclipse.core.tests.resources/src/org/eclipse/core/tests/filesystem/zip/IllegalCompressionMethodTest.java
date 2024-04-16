a70e6578028b237453393354c1efa55f63605b04
CodeLtDave <david.erdoes@vector.com>
2024-04-16 15:57:31 +0200

    Adjusted Illegal Compression behavior and test

--- resources/tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/filesystem/zip/IllegalCompressionMethodTest.java
+++ resources/tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/filesystem/zip/IllegalCompressionMethodTest.java
@@ -14,11 +14,14 @@
 package org.eclipse.core.tests.filesystem.zip;
 
 import static org.eclipse.core.tests.filesystem.zip.ZipFileSystemTestUtil.ensureExists;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ZipExpander;
 import org.eclipse.core.runtime.CoreException;
 import org.junit.After;
 import org.junit.Before;
@@ -44,14 +47,17 @@ public class IllegalCompressionMethodTest {
 
 	@Test
 	public void testExpandEnhancedDeflatedArchive() throws CoreException, URISyntaxException, IOException {
-		IFile archiveFile = ZipFileSystemTestSetup.firstProject.getFile(ARCHIVE_NAME);
+		IProject project = ZipFileSystemTestSetup.firstProject;
+		IFile archiveFile = project.getFile(ARCHIVE_NAME);
+
 		ensureExists(archiveFile);
-		try {
-			ZipFileSystemTestUtil.expandZipFile(archiveFile);
-		} catch (IOException e) {
-			assertEquals("invalid CEN header (unsupported compression method: 9)",
-					e.getMessage());
-		}
+		ZipExpander.expandZip(archiveFile);
+
+		ensureExists(archiveFile);
+		IFolder archiveFolder = project.getFolder(ARCHIVE_NAME);
+		assertFalse(
+				"Error: A folder named '\" + ARCHIVE_NAME + \"' exists which should not. The zip should have been collapsed against\"",
+				archiveFolder.exists());
 	}
 }
 
