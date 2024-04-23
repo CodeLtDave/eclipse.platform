87bb3480713b13aa415c10732ee91669c29b6526
Michael Bangas <michael.bangas@vector.com>
2024-04-23 13:23:11 +0200

    Merge ZipExpander and ZipCollapser to one class ZipTransformer

--- resources/bundles/org.eclipse.core.resources/src/org/eclipse/core/resources/ZipTransformer.java
+++ resources/bundles/org.eclipse.core.resources/src/org/eclipse/core/resources/ZipTransformer.java
@@ -6,12 +6,13 @@ import org.eclipse.core.filesystem.EFS;
 import org.eclipse.core.filesystem.IFileStore;
 import org.eclipse.core.filesystem.URIUtil;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
 
 /**
  * @since 3.21
  *
  */
-public class ZipCollapser {
+public class ZipTransformer {
 	public static void collapseZip(IFolder folder) throws URISyntaxException, CoreException {
 		URI zipURI = new URI(folder.getLocationURI().getQuery());
 		IFileStore parentStore = EFS.getStore(folder.getParent().getLocationURI());
@@ -19,6 +20,17 @@ public class ZipCollapser {
 		if (URIUtil.equals(zipURI, childURI)) {
 			folder.delete(IResource.COLLAPSE, null);
 			folder.getProject().refreshLocal(IResource.DEPTH_INFINITE, null);
+		}
+	}
+
+	public static void expandZip(IFile file) throws URISyntaxException, CoreException {
+		URI zipURI = new URI("zip", null, "/", file.getLocationURI().toString(), null); //$NON-NLS-1$ //$NON-NLS-2$
+		IFolder link = file.getParent().getFolder(IPath.fromOSString(file.getName()));
+		link.createLink(zipURI, IResource.REPLACE, null);
+	
+		// Roleback if Folder "link" is empty
+		if (link.exists() && link.members().length == 0) {
+			collapseZip(link);
 		}
 	}
 }
